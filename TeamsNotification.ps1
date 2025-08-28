99f28a5935a38c94298a7f79cf68c7d32a04dcd5
Nathan Taylor <ntaylor0377@gmail.com>
2025-08-28 13:52:19 +1000

    Refactor TeamsNotificaiton
    Teams notification now uses Adaptive Cards. Previous webhook was deprecated.

--- TeamsNotification.ps1
+++ TeamsNotification.ps1
@@ -13,25 +13,54 @@ class TeamsNotification
 
         $jsonText = @"
         {
-            "@context": "https://schema.org/extensions",
-            "@type": "MessageCard",
-            "summary": "Nest found without TUSARC5 applied!",
-            "themeColor": "a78bfa",
-            "title": "Nest found without TUSARC5 applied, ${lstName}",
-            "sections": [{
-                "activityImage": "https://media.giphy.com/media/mz6aYifIzDqOuZSvEd/giphy.gif",
-                "facts": [
-                    {
-                        "name": "Nest name",
-                        "value": "${lstName}"
-                    },
-                    {
-                        "name": "Checked by",
-                        "value": "${checkedByUser}"
+            "type": "message",
+            "attachments": [
+                {
+                    "contentType": "application/vnd.microsoft.card.adaptive",
+                    "content": {
+                        "type": "AdaptiveCard",
+                        "`$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "version": "1.5",
+                        "body": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Nest found without TUSARC5 applied, ${lstName}",
+                                "wrap": true,
+                                "weight": "Bolder"
+                            },
+                            {
+                                "type": "Image",
+                                "url": "https://media.giphy.com/media/mz6aYifIzDqOuZSvEd/giphy.gif",
+                                "size": "Medium"
+                            },
+                            {
+                                "type": "FactSet",
+                                "facts": [
+                                    {
+                                        "title": "Nest name",
+                                        "value": "${lstName}"
+                                    },
+                                    {
+                                        "title": "Check by",
+                                        "value": "${checkedByUser}"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "This nest will have to be re-nc'd with the TUSARC5 post processor.",
+                                "wrap": true
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Sent from the [Tusarc-Processor](https://github.com/ntaylor-86/Tusarc-Processor) running on `GCI-TRUMPF`.",
+                                "wrap": true,
+                                "size": "Small"
+                            }
+                        ]
                     }
-                ],
-                "text": "This nest will have to be re-nc'd with the TUSARC5 post processor."
-            }]
+                }
+            ]
         }
 "@
         
